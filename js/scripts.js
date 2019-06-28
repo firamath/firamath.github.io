f9094e652c00008ee8c4de54b0c8e676774338aa
Xiangdong Zeng <xdzeng96@gmail.com>
2019-06-28 17:29:04 +0800

    use eslint

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,21 @@
-"use strict";
+'use strict';
 
-// var bodyElement = document.querySelector("body");
-// var scrollBarWidth = window.innerWidth - document.documentElement.clientWidth;
-// var marginLeft = parseFloat(window.getComputedStyle(bodyElement).marginLeft);
-// bodyElement.setAttribute("style", `margin-left: ${String(marginLeft + scrollBarWidth / 2)}px`);
+const filename = window.location.href.split('/').pop();
 
-var filename = window.location.href.split("/").pop();
+const navTags = {
+  index:    'Home',
+  specimen: 'Specimen',
+  lab:      'Lab',
+};
 
-var navTags = {
-    "index.html":    "Home",
-    "specimen.html": "Specimen",
-    "lab.html":      "Lab"
+let navHTML = '<ul class="toc-links">';
+for (const i of Object.keys(navTags)) {
+  const navName = `<a href='${i}.html'>${navTags[i]}</a>`;
+  if (i + '.html' === filename) {
+    navHTML += `<li class='selected'>${navName}</li>`;
+  } else {
+    navHTML += `<li>${navName}</li>`;
+  }
 }
-
-var navHTML = "<ul class='toc-links'>";
-for (var i in navTags) {
-    var navName = `<a href='${i}'>${navTags[i]}</a>`;
-    if (i == filename)
-        navHTML += `<li class='selected'>${navName}</li>`;
-    else
-        navHTML += `<li>${navName}</li>`;
-}
-navHTML += "</ul>";
-document.querySelector("nav").innerHTML = navHTML;
+navHTML += '</ul>';
+document.querySelector('nav').innerHTML = navHTML;
--- utils/char.py
+++ utils/char.py
@@ -1,16 +1,18 @@
-from __future__ import print_function
 import os
 import fontforge as ff
 
 
 WEIGHT_LIST = ["Thin", "UltraLight", "ExtraLight", "Light", "Book", "Regular",
                "Medium", "SemiBold", "Bold", "ExtraBold", "Heavy", "Ultra"]
+ESLINT_HEADER = "/* eslint-disable key-spacing, max-len, comma-spacing */"
 
-entry = []
+file_str = ESLINT_HEADER + "\n"
+file_str += "const firaMathChar = {\n"
 for weight in WEIGHT_LIST:
-    font = ff.open(os.sep.join([os.getcwd(), "assets", "FiraMath-" + weight + ".otf"]))
+    font = ff.open(os.sep.join([os.getcwd(), "assets", "FiraMath-" + weight + ".woff"]))
     unicode_list = [str(i.unicode) for i in font.glyphs() if i.unicode >= 0]
-    entry.append("    \"" + font.fontname + "\": [" + ",".join(unicode_list) + "]")
+    file_str += "  '{key}': [{items}],\n".format(key=font.fontname, items=",".join(unicode_list))
     font.close()
+file_str += "};"
 
-print("var firamathChar = {\n" + ",\n".join(entry) + "\n};")
+print(file_str)
